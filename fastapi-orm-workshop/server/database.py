28f992cb7b48e0589c3d4daf91799668b0a1bd8d
ShonyALV <ljhonyalfonso@gmail.com>
2026-09-18 08:45:53 -0500

    server/database.py file creation

---
+++ fastapi-orm-workshop/server/database.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+
+DATABASE_URL = (
+    "mysql+pymysql://ormuser:ormpass123@localhost/orm_workshop"
+)
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+
+    db = SessionLocal()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
